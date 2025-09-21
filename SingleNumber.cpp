d18922e4fdea322aa479715555eb572847853a50
Sable Ayala <sable262021@gmail.com>
2025-09-21 16:21:56 -0400

    single number cpp

---
+++ SingleNumber.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class Solution
+{
+public:
+  int singleNumber(std::vector<int> &nums)
+  {
+    int res = 0;
+    for (int num : nums)
+    {
+      res ^= num;
+    }
+
+    return res;
+  }
+};
