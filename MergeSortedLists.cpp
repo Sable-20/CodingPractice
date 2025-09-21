88ab3c42b750766e02a217e9d05d3b9c5a7cdb89
Sable Ayala <sable262021@gmail.com>
2025-09-21 15:58:20 -0400

    second problem

---
+++ MergeSortedLists.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+  void merge(std::vector<int> &nums1, int m, std::vector<int> &nums2, int n)
+  {
+    for (int j = 0, i = m; j < n; j++)
+    {
+      nums1[i] = nums2[j];
+      i++;
+    }
+    std::sort(nums1.begin(), nums1.end());
+  }
+};
